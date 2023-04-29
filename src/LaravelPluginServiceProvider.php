91ee00d1839951944e45679291d03f38b20cf953
Hatsuhito UENO <ueno@uehatsu.info>
2023-04-29 15:38:27 +0900

    First commit.

---
+++ src/LaravelPluginServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Uehatsu\LaravelCleanArchitecture;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPluginServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
