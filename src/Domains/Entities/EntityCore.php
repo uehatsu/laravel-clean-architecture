7f270c3bdef020f3e42d64f4b5e5d81ba8c9da8e
Hatsuhito UENO <ueno@uehatsu.info>
2023-05-04 22:57:34 +0900

    Added EntityCore interface file.

---
+++ src/Domains/Entities/EntityCore.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Uehatsu\LaravelCleanArchitecture\Domains\Entities;
+
+/**
+ * EntityCore Interface
+ */
+interface EntityCore
+{
+    /**
+     * @template T of EntityCore
+     * @param T $other
+     * @return bool
+     */
+    public function equals($other): bool;
+}
